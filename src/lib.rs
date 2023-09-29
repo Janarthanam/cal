158ff4814acb8ef4ec9882d93df088847849c482
Janar Ramalingam <janarthanam.r@gmail.com>
2023-09-29 13:22:03 +0530

    Add test for update
    Fix upsert interface to return slot
    to avoid get to update again

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod cal;
 mod tests {
     use std::time::{Duration, SystemTime, UNIX_EPOCH};
     use crate::cal::{Calendar, Event, Slot};
-    use crate::cal::Status::Attending;
+    use crate::cal::Status::{Attending, Tentative};
 
     #[test]
     pub fn get_empty() {
@@ -48,5 +48,33 @@ mod tests {
         });
         assert_eq!(1, events.len());
         assert_eq!(expected.start_time, events[0].0.start_time)
+    }
+
+    #[test]
+    pub fn update() -> () {
+        let mut cal = Calendar::new();
+
+        //create an event
+        let scheduled = cal.upsert(Slot {
+            start_time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
+            end_time: SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
+            ..Slot::default()
+        }, Event{
+            status: Attending,
+            description: Some(String::from("Test Event")),
+            title: Some(String::from("Test title"))
+        });
+
+        assert!(scheduled.is_some());
+        assert!(scheduled.as_ref().unwrap().id.is_some());
+
+        let tentative = cal.upsert(scheduled.unwrap(), Event {
+            status: Tentative,
+            description: Some(String::from("Test Event is tentative")),
+            title: Some(String::from("Test title - tentative"))
+        });
+
+        assert!(tentative.is_some());
+        assert!(tentative.unwrap().id.is_some());
     }
 }
